35518fab3181da19360eef2fdf6eed11db280504
Kevin A. <17864518+Kmagameguy@users.noreply.github.com>
2025-09-26 10:45:46 -0400

    Update Extensions List

--- script/vscode-extensions.sh
+++ script/vscode-extensions.sh
@@ -3,10 +3,14 @@
 
 declare -a packages=(
  'bierner.emojisense'
- 'EditorConfig.EditorConfig'
+ 'bierner.markdown-mermaid'
  'file-icons.file-icons'
- 'HookyQR.beautify'
- 'timonwong.shellcheck'
+ 'kaiwood.endwise'
+ 'ms-azuretools.vscode-docker'
+ 'ms-vscode-remote.remote-containers'
+ 'ms-vscode-remote.remote-explorer'
+ 'ms-vscode-remote.remote-ssh'
+ 'ms-vscode-remote.remote-ssh-edit'
 )
 
 for extension in "${packages[@]}"; do
